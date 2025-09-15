b41d9d10c237b7a2509919e197262c8a49e092e8
Morgan <macbook@Metalysts-MacBook-Pro.local>
2025-09-15 21:39:09 +0800

    Add upload_excel route & upload page

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use App\Service\ExcelImportService;
@@ -10,36 +11,22 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class ReportController extends AbstractController
 {
-    #[Route('/upload', name: 'app_upload')]
-    public function upload(Request $request, ExcelImportService $importer): Response
+    #[Route('/upload', name: 'upload_excel')]
+    public function upload(Request $request, ExcelImportService $excelImportService): Response
     {
-        $message = null;
-
         if ($request->isMethod('POST')) {
-            $file = $request->files->get('excel');
-            if ($file) {
-                $uploadsDir = $this->getParameter('kernel.project_dir').'/var/uploads';
-                if (!is_dir($uploadsDir)) {
-                    mkdir($uploadsDir, 0777, true);
-                }
+            $file = $request->files->get('excel_file');
 
-                $filepath = $uploadsDir.'/'.$file->getClientOriginalName();
+            if ($file) {
                 try {
-                    $file->move($uploadsDir, $file->getClientOriginalName());
-                    $count = $importer->import($filepath);
-                    $message = "Амжилттай: $count мөр импортлогдлоо ✅";
-                } catch (FileException $e) {
-                    $message = "Файл хадгалах үед алдаа: ".$e->getMessage();
-                } catch (\Throwable $e) {
-                    $message = "Импорт алдаа: ".$e->getMessage();
+                    $excelImportService->import($file->getPathname());
+                    $this->addFlash('success', 'Excel амжилттай импортлогдлоо!');
+                } catch (\Exception $e) {
+                    $this->addFlash('error', 'Алдаа гарлаа: '.$e->getMessage());
                 }
-            } else {
-                $message = "Excel файл сонгоно уу!";
             }
         }
 
-        return $this->render('report/upload.html.twig', [
-            'message' => $message,
-        ]);
+        return $this->render('report/upload.html.twig');
     }
 }
