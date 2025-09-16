ccf55bdf647217f7319f314fa795514152b123bd
Morgan <macbook@Metalysts-MacBook-Pro.local>
2025-09-16 10:23:40 +0800

    Fix: properly close braces in ReportController

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -7,33 +7,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class ReportController extends AbstractController
 {
     #[Route('/upload', name: 'upload_excel')]
-public function upload(Request $request, ExcelImportService $excelImportService): Response
-{
-    if ($request->isMethod('POST')) {
-        $file = $request->files->get('excel_file');
+    public function upload(Request $request, ExcelImportService $excelImportService): Response
+    {
+        if ($request->isMethod('POST')) {
+            $file = $request->files->get('excel_file');
 
-        if (!$file) {
-            $this->addFlash('error', 'Файл сонгогдоогүй.');
+            if (!$file) {
+                $this->addFlash('error', 'Файл сонгогдоогүй.');
+                return $this->redirectToRoute('upload_excel');
+            }
+
+            try {
+                // Импортын сервис мөрийн тоо буцаана
+                $count = $excelImportService->import($file->getPathname());
+                $this->addFlash('success', sprintf('Excel амжилттай импортлогдлоо! (%d мөр)', $count));
+            } catch (\Throwable $e) {
+                $this->addFlash('error', 'Алдаа: '.$e->getMessage());
+            }
+
+            // PRG (Post/Redirect/Get)
             return $this->redirectToRoute('upload_excel');
         }
 
-        try {
-            // Импортын сервисээ мөрийн тоо буцаадаг болгодог (доорх 3-р алхам)
-            $count = $excelImportService->import($file->getPathname());
-            $this->addFlash('success', sprintf('Excel амжилттай импортлогдлоо! (%d мөр)', $count));
-        } catch (\Throwable $e) {
-            $this->addFlash('error', 'Алдаа: '.$e->getMessage());
-        }
-
-        // PRG (Post/Redirect/Get)
-        return $this->redirectToRoute('upload_excel');
+        return $this->render('report/upload.html.twig');
     }
-
-    return $this->render('report/upload.html.twig');
 }
-
