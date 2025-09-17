04a07ee7c181c8dacac6631f406ffcf9684f5048
Morgan <macbook@Metalysts-MacBook-Pro.local>
2025-09-17 21:47:45 +0800

    ux: redirect / to report if logged in, else to login

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,12 +1,31 @@
 <?php
 namespace App\Controller;
+
+use App\Repository\TransactionRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Twig\Environment;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-class HomeController {
+#[IsGranted('ROLE_USER')]
+final class HomeController extends AbstractController
+{
     #[Route('/', name: 'app_home')]
-    public function __invoke(Environment $twig): Response {
-        return new Response($twig->render('home/index.html.twig', ['title'=>'MicroFin']));
+    public function __invoke(TransactionRepository $repo): Response
+    {
+        $u = $this->getUser();
+
+        $transactions = $repo->createQueryBuilder('t')
+            ->andWhere('t.user = :u')->setParameter('u', $u)
+            ->orderBy('t.date', 'DESC')
+            ->addOrderBy('t.id', 'DESC')
+            ->setMaxResults(50)
+            ->getQuery()
+            ->getResult();
+
+        return $this->render('home/index.html.twig', [
+            'title' => 'MicroFin',
+            'transactions' => $transactions,
+        ]);
     }
 }
