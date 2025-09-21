f8c64513f684ef3fbd643c2ae042e5c3afc2a40f
Morgan <macbook@Metalysts-MacBook-Pro.local>
2025-09-21 13:13:03 +0800

    feat: add MonologBundle and enable security debug logs in prod

--- config/bundles.php
+++ config/bundles.php
@@ -4,10 +4,8 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-
-    // Зөвхөн dev/test дээр идэвхтэй (prod дээр биш)
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
